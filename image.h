3a56e59bfa64d3873934ee20e9a58fbbf8d72935
Joseph Neville <Wenadin21@gmail.com>
2012-02-10 19:08:13 -0800

    Update 1

--- image.h
+++ image.h
@@ -9,10 +9,8 @@ public:
 	Image(int , int , int);
 	Image(const Image &);
 	~Image();
-	void readImage(char *, Image &);
-	void writeImage(char *, const Image &);
 	void getImageInfo(int &, int &, int &);
-	int getPixelVal(int, int);
+	void getPixelVal(int, int, int &);
 	void setPixelVal(int, int, int);
 	void getSubImage(int, int, int, int, Image &);
 	int meanGray();
@@ -30,6 +28,8 @@ private:
 	int **pixelVal;
 };
 
+	void readImage(char *, Image &);
+	void writeImage(char *, Image &);
 #endif
 	
 	
