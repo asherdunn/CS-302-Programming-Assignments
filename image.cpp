1706b9df44302f369302d6d1fa188cf1a42eff3b
Asher Dunn <asherd@me.com>
2012-02-13 20:26:41 -0800

    Spacing corrected for clearity.
    Signed-off-by: Asher Dunn <asherd@me.com>

--- image.cpp
+++ image.cpp
@@ -25,33 +25,38 @@ ImageType::ImageType(int tmpN, int tmpM, int tmpQ)
  Q = tmpQ;
 
  pixelValue = new int* [N];
- for(i=0; i<N; i++) {
+ for(i=0; i<N; i++)
+ {
    pixelValue[i] = new int[M];
    for(j=0; j<M; j++)
-     pixelValue[i][j] = 0;
+   {
+        pixelValue[i][j] = 0;
+   }
  }
 }
 
 ImageType::~ImageType()
 {
 	for (int colCount=0; colCount < N; colCount++)
+    {
 		delete [] pixelValue [colCount];
+    }
 	delete [] pixelValue;
 
 }
 
 void ImageType::getImageInfo(int& rows, int& cols, int& levels)
 {
- rows = N;
- cols = M;
- levels = Q;
+    rows = N;
+    cols = M;
+    levels = Q;
 } 
 
 void ImageType::setImageInfo(int rows, int cols, int levels)
 {
- N= rows;
- M= cols;
- Q= levels;
+    N= rows;
+    M= cols;
+    Q= levels;
 } 
 
 void ImageType::setPixelVal(int i, int j, int pixelV)
@@ -74,166 +79,182 @@ void ImageType::Threshold(ImageType& image)
  // threshold image 
 
  for(int i=0; i<N; i++)
-   for(int j=0; j<M; j++) 
-   {
-     image.getPixelVal(i, j, pixelV);
-     if(pixelV < thresh) 
-       image.setPixelVal(i, j, 255);
-     else
-       image.setPixelVal(i, j, 0);
+    for(int j=0; j<M; j++) 
+    {
+    image.getPixelVal(i, j, pixelV);
+    if(pixelV < thresh) 
+        image.setPixelVal(i, j, 255);
+    else
+        image.setPixelVal(i, j, 0);
     }
     
 }
 
 void readImageHeader(char fname[], int& N, int& M, int& Q, bool& type)
 {
- int i, j;
- unsigned char *charImage;
- char header [100], *ptr;
- ifstream ifp;
+    int i, j;
+    unsigned char *charImage;
+    char header [100], *ptr;
+    ifstream ifp;
 
- ifp.open(fname, ios::in | ios::binary);
+    ifp.open(fname, ios::in | ios::binary);
 
- if (!ifp) {
-   cout << "Can't read image: " << fname << endl;
-   exit(1);
- }
+    if (!ifp) 
+    {
+    cout << "Can't read image: " << fname << endl;
+    exit(1);
+    }
 
  // read header
 
- type = false; // PGM
+    type = false; // PGM
 
- ifp.getline(header,100,'\n');
- if ( (header[0] == 80) &&  /* 'P' */
-      (header[1]== 53) ) {  /* '5' */
-      type = false;
- }
- else if ( (header[0] == 80) &&  /* 'P' */
-      (header[1] == 54) ) {        /* '6' */
-      type = true;
- } 
- else {
-   cout << "Image " << fname << " is not PGM or PPM" << endl;
-   exit(1);
- }
+    ifp.getline(header,100,'\n');
+    if ( (header[0] == 80) &&  /* 'P' */
+         (header[1]== 53) ) 
+        {  /* '5' */
+            type = false;
+        }
+    else if ( (header[0] == 80) &&  /* 'P' */
+              (header[1] == 54) ) 
+            {
+                /* '6' */
+                type = true;
+            } 
+    else 
+    {
+        cout << "Image " << fname << " is not PGM or PPM" << endl;
+        exit(1);
+    }
 
-ifp.getline(header,100,'\n');
- while(header[0]=='#')
-   ifp.getline(header,100,'\n');
+    ifp.getline(header,100,'\n');
+    while(header[0]=='#')
+        ifp.getline(header,100,'\n');
 
- M=strtol(header,&ptr,0);
- N=atoi(ptr);
+    M=strtol(header,&ptr,0);
+    N=atoi(ptr);
 
- ifp.getline(header,100,'\n');
+    ifp.getline(header,100,'\n');
 
- Q=strtol(header,&ptr,0);
+    Q=strtol(header,&ptr,0);
 
- ifp.close();
+    ifp.close();
 
 }
 
 void readImage(char fname[], ImageType& image)
 {
- int i, j;
- int N, M, Q;
- unsigned char *charImage;
- char header [100], *ptr;
- ifstream ifp;
+    int i, j;
+    int N, M, Q;
+    unsigned char *charImage;
+    char header [100], *ptr;
+    ifstream ifp;
 
- ifp.open(fname, ios::in | ios::binary);
+    ifp.open(fname, ios::in | ios::binary);
 
- if (!ifp) {
-   cout << "Can't read image: " << fname << endl;
-   exit(1);
- }
+    if (!ifp) 
+    {
+        cout << "Can't read image: " << fname << endl;
+        exit(1);
+    }
 
  // read header
 
- ifp.getline(header,100,'\n');
- if ( (header[0]!=80) ||    /* 'P' */
-      (header[1]!=53) ) {   /* '5' */
-      cout << "Image " << fname << " is not PGM" << endl;
-      exit(1);
- }
+    ifp.getline(header,100,'\n');
+    if ( (header[0]!=80) ||    /* 'P' */
+         (header[1]!=53) ) 
+    {   /* '5' */
+            cout << "Image " << fname << " is not PGM" << endl;
+            exit(1);
+    }
 
-ifp.getline(header,100,'\n');
- while(header[0]=='#')
-   ifp.getline(header,100,'\n');
+    ifp.getline(header,100,'\n');
+    while(header[0]=='#')
+        ifp.getline(header,100,'\n');
 
- M=strtol(header,&ptr,0);
- N=atoi(ptr);
+    M=strtol(header,&ptr,0);
+    N=atoi(ptr);
 
- ifp.getline(header,100,'\n');
- Q=strtol(header,&ptr,0);
+    ifp.getline(header,100,'\n');
+    Q=strtol(header,&ptr,0);
 
- charImage = (unsigned char *) new unsigned char [M*N];
+    charImage = (unsigned char *) new unsigned char [M*N];
 
- ifp.read( reinterpret_cast<char *>(charImage), (M*N)*sizeof(unsigned char));
+    ifp.read( reinterpret_cast<char *>(charImage), (M*N)*sizeof(unsigned char));
 
- if (ifp.fail()) {
-   cout << "Image " << fname << " has wrong size" << endl;
-   exit(1);
- }
+    if (ifp.fail()) 
+    {
+        cout << "Image " << fname << " has wrong size" << endl;
+        exit(1);
+    }
 
- ifp.close();
+    ifp.close();
 
  //
  // Convert the unsigned characters to integers
  //
 
- int val;
+    int val;
 
- for(i=0; i<N; i++)
-   for(j=0; j<M; j++) {
-     val = (int)charImage[i*M+j];
-     image.setPixelVal(i, j, val);     
-   }
+    for(i=0; i<N; i++)
+    {
+        for(j=0; j<M; j++) 
+        {
+            val = (int)charImage[i*M+j];
+            image.setPixelVal(i, j, val);     
+        }
+    }
 
- delete [] charImage;
+    delete [] charImage;
 
 }
 
 void writeImage(char fname[], ImageType& image)
 {
- int i, j;
- int N, M, Q;
- unsigned char *charImage;
- ofstream ofp;
+    int i, j;
+    int N, M, Q;
+    unsigned char *charImage;
+    ofstream ofp;
 
- image.getImageInfo(N, M, Q);
+    image.getImageInfo(N, M, Q);
 
- charImage = (unsigned char *) new unsigned char [M*N];
+    charImage = (unsigned char *) new unsigned char [M*N];
 
  // convert the integer values to unsigned char
 
- int pixelV;
+    int pixelV;
 
- for(i=0; i<N; i++)
-   for(j=0; j<M; j++) {
-     image.getPixelVal(i, j, pixelV);
-     charImage[i*M+j]=(unsigned char)pixelV;
-   }
+    for(i=0; i<N; i++)
+    {
+        for(j=0; j<M; j++) 
+        {
+            image.getPixelVal(i, j, pixelV);
+            charImage[i*M+j]=(unsigned char)pixelV;
+        }
+    }
 
- ofp.open(fname, ios::out | ios::binary);
+    ofp.open(fname, ios::out | ios::binary);
 
- if (!ofp) {
-   cout << "Can't open file: " << fname << endl;
-   exit(1);
- }
+    if (!ofp) 
+    {
+        cout << "Can't open file: " << fname << endl;
+        exit(1);
+    }
 
- ofp << "P5" << endl;
- ofp << M << " " << N << endl;
- ofp << Q << endl;
+    ofp << "P5" << endl;
+    ofp << M << " " << N << endl;
+    ofp << Q << endl;
 
- ofp.write( reinterpret_cast<char *>(charImage), (M*N)*sizeof(unsigned char));
+    ofp.write( reinterpret_cast<char *>(charImage), (M*N)*sizeof(unsigned char));
 
- if (ofp.fail()) {
-   cout << "Can't write image " << fname << endl;
-   exit(0);
- }
+    if (ofp.fail()) 
+    {
+        cout << "Can't write image " << fname << endl;
+        exit(0);
+    }
 
- ofp.close();
+    ofp.close();
 
- delete [] charImage;
+    delete [] charImage;
 
 }
